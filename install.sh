56a3b4a2d5d84eacb1b59febc129e86920b577f8
CrystalSpore <sporeluc@gmail.com>
2023-04-07 14:08:03 -0500

    modified install script, hopefully works on more systems

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 current_dir=$(pwd)
-source_dir=$(dirname -- "$0";)
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd $source_dir
+cd $script_dir
 echo "Files being \"installed\" (symlinked)"
 
 # Info for below find command, by line:
@@ -12,11 +12,11 @@ echo "Files being \"installed\" (symlinked)"
 # don't include install script
 # don't include .gitignore file
 # don't include git directory
-# print files being "installed" && create directories if missing && remove any old files manually created && symlink the files"
+# print files being "installed" && remove any old files manually created && symlink the files"
 find .* -type f \
 	 -not -name "*~" \
 	 -not -name "install.sh" \
 	 -not -name ".gitignore" \
 	 -not -path ".git/*" \
-		  -exec sh -c 'echo "$HOME/$0" && mkdir -p "$HOME/{0%/*}" && rm "$HOME/$0" && ln -s "$(pwd)/$0" "$HOME/$0"' {} \;
+		  -exec bash -c 'echo "$HOME/$0" && rm -f "$HOME/$0" && ln -s "$(pwd)/$0" "$HOME/$0"' {} \;
 cd $current_dir
