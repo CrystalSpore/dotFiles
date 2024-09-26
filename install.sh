7fce0b231fb8e42272c5e479a1dc3e9ec33d75e1
CrystalSpore <sporeluc@gmail.com>
2024-09-26 02:02:24 +0000

    add tmux files & install script creates directories

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ echo "Files being \"installed\" (symlinked)"
 # Info for below find command, by line:
 # include only files
 # don't include emacs temp files
-# print files being "installed" && remove any old files manually created && symlink the files"
+# print files being "installed" && create directories to where files should live && remove any old files manually created && symlink the files"
 find -type f \
-     -not -name "*~" \
+	 -not -name "*~" \
      -not -name "#*#" \
-		  -exec bash -c 'file_path="$(echo ${0:2})" && echo "$HOME/$file_path" && rm -f "$HOME/$file_path" && ln -s "$(pwd)/$file_path" "$HOME/$file_path"' {} \;
+	      -exec bash -c 'file_path="$(echo ${0:2})" && dir_path="$(dirname $file_path)" && mkdir -p "$HOME/$dir_path" && echo "$HOME/$file_path" && rm -f "$HOME/$file_path" && ln -s "$(pwd)/$file_path" "$HOME/$file_path"' {} \;
 cd $current_dir
